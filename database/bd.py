d239c0a7ccbad769699149f31e80204f6aee8b46
Андрей Самойлов <eeorroll@gmail.com>
2025-02-20 18:47:00 +0200

    Update bd.py

--- database/bd.py
+++ database/bd.py
@@ -10,11 +10,11 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
+cursor.execute("mydatabase")
 cursor.execute("USE mydatabase")
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
+   users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         login VARCHAR(100) UNIQUE,
         password VARCHAR(255),
@@ -26,11 +26,6 @@ cursor.execute('''
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-cursor.execute("INSERT INTO users (login, password, phone_number, token) VALUES (%s, %s, %s, %s)", 
-               ("ivan123", hash_password("mypassword"), "1234567890", "sampletoken123"))
-cursor.execute("INSERT INTO users (login, password, phone_number, token) VALUES (%s, %s, %s, %s)", 
-               ("maria456", hash_password("securepass"), "0987654321", "anothertoken456"))
-conn.commit()  
 
 cursor.execute("SELECT * FROM users")
 for row in cursor.fetchall():
